12af3834f427b2294331922baa02e05aadd9da25
Salomon BRYS <salomon@kodein.net>
2020-06-19 15:33:47 +0200

    Workaround for Android 4.4 & lower

--- kodein-type/src/jvmMain/kotlin/org/kodein/type/JVMAbstractTypeToken.kt
+++ kodein-type/src/jvmMain/kotlin/org/kodein/type/JVMAbstractTypeToken.kt
@@ -1,6 +1,6 @@
 package org.kodein.type
 
-import java.lang.reflect.Type
+import java.lang.reflect.*
 
 internal abstract class JVMAbstractTypeToken<T> : TypeToken<T>() {
 
@@ -11,9 +11,66 @@ internal abstract class JVMAbstractTypeToken<T> : TypeToken<T>() {
 
     final override fun typeEquals(other: TypeToken<*>): Boolean {
         require(other is JVMAbstractTypeToken<*>)
-        if (jvmType != other.jvmType) return false
-        return true
+
+        return Equals(jvmType, other.jvmType)
     }
 
-    final override fun typeHashCode(): Int = jvmType.hashCode()
+    final override fun typeHashCode(): Int = HashCode(jvmType)
+
+    companion object {
+        private abstract class WrappingTest<T> {
+            val type: Type get() = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
+        }
+
+        private val needPTWorkaround: Boolean by lazy {
+            val t1 = (object : WrappingTest<List<String>>() {}).type as ParameterizedType
+            val t2 = (object : WrappingTest<List<String>>() {}).type as ParameterizedType
+            t1 != t2
+        }
+
+        private val needGATWorkaround: Boolean by lazy {
+            val t1 = (object : WrappingTest<Array<List<String>>>() {}).type as GenericArrayType
+            val t2 = (object : WrappingTest<Array<List<String>>>() {}).type as GenericArrayType
+            t1 != t2
+        }
+
+        fun HashCode(type: Type): Int =
+            when {
+                needPTWorkaround && type is ParameterizedType -> {
+                    var hashCode = HashCode(type.rawType)
+                    for (arg in type.actualTypeArguments)
+                        hashCode = hashCode * 31 + HashCode(arg)
+                    hashCode
+                }
+                needGATWorkaround && type is GenericArrayType -> {
+                    53 + HashCode(type.genericComponentType)
+                }
+                else -> type.hashCode()
+            }
+
+        fun Equals(left: Type, right: Type): Boolean {
+            if (left.javaClass != right.javaClass)
+                return false
+
+            return when {
+                needPTWorkaround && left is ParameterizedType -> {
+                    right as ParameterizedType
+                    Equals(left.rawType, right.rawType) && Equals(left.actualTypeArguments, right.actualTypeArguments)
+                }
+                needGATWorkaround && left is GenericArrayType -> {
+                    right as GenericArrayType
+                    Equals(left.genericComponentType, right.genericComponentType)
+                }
+                else -> left == right
+            }
+        }
+
+        fun Equals(left: Array<Type>, right: Array<Type>): Boolean {
+            if (left.size != right.size)
+                return false
+            return left.indices.none { !Equals(left[it], right[it]) }
+        }
+
+
+    }
 }
