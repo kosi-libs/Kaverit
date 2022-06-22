17e3e103f180182774f587c541c86f49daf318ed
Romain BOISSELLE <romain.boisselle@proton.me>
2022-06-22 12:58:25 +0200

    Move to KOSI-libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ import org.kodein.internal.gradle.settings.android
 buildscript {
     repositories {
         mavenLocal()
-        maven(url = "https://raw.githubusercontent.com/Kodein-Framework/kodein-internal-gradle-plugin/mvn-repo")
+        maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
         classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:6.18.0")
