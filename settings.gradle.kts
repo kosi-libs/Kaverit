4500a68283dbf9ef1bd178efcd8e5b43e7898f17
Romain BOISSELLE <romain.boisselle@proton.me>
2022-09-29 19:43:17 +0200

    Kotlin 1.7.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ import org.kodein.internal.gradle.settings.android
         maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:6.20.2-kotlin-1.7.20-RC")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:6.21.0")
     }
 }
 
